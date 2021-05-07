2b65869a44f6b6e2079d905d9cddca17731f6e13
grapette <igorpompeuzs@yahoo.com.br>
2021-05-06 23:02:37 -0300

    classe Banco

---
+++ src/main/kotlin/Banco.kt
@@ -0,0 +1,5 @@
+package main.kotlin
+
+data class Banco(val nome:String, val numero:Int){
+
+}
---
+++ src/main/kotlin/testes/BancoTeste.kt
@@ -0,0 +1,8 @@
+package main.kotlin.testes
+
+import main.kotlin.Banco
+
+fun main(){
+    val digiOneBank = Banco("DigiOne",12)
+    println(digiOneBank.nome)
+}

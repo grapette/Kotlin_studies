c531f89dc4b40b5b0569b583d0e546b6dd6e42e3
grapette <igorpompeuzs@yahoo.com.br>
2021-05-06 21:56:38 -0300

    atualizacao projeto - classe Pessoa

---
+++ src/main/kotlin/Pessoa.kt
@@ -0,0 +1,11 @@
+package main.kotlin
+
+class Pessoa {
+    var nome: String = "Igor"
+    var cpf: String = "000.123.456-78"
+}
+fun main() {
+    val Igor =Pessoa()
+    println(Igor.nome)
+    println(Igor.cpf)
+}
